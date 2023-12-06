8f74cd36f54954a9f3f12681e4b18795b64615e9
Sahil <sahilkumbharkar57@gmail.com>
2023-12-07 01:10:39 +0530

    s

--- src/app.js
+++ src/app.js
@@ -4,48 +4,26 @@ const serverless = require('serverless-http')
 const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 8000
+const router = express.Router();
 
 const staticpath = path.join(__dirname, '../public')
 app.use(express.static(staticpath))
 
 var value;
-app.get('/', (req, res) => {
-    res.send("Hi")
+router.get('/', (req, res) => {
+    res.write("Hi")
+    res.end()
 })
-app.get('/about', (req, res) => {
-    res.send("Hi from About")
-})
-
-app.get('/form_submit', (req, res) => {
-    value = req.query.Email;
-    res.send("Send Succesfully")
-    main(value);
-
+router.get('/about', (req, res) => {
+    res.write("Hi from About")
+    res.end()
 })
 
 app.listen(PORT, () => {
     console.log("Server was Started")
 })
-
-async function main(value) {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        auth: {
-            user: "sahilkumbharkar57@gmail.com",
-            pass: "ozlk nqtf dsgm cubl",
-        },
-    });
-
-    const content = {
-        from: 'sahilkumbharkar57@gmail.com',
-        to: `${value}`,
-        subject: "   THANK YOU !!   ",
-        text: "   Thank you For Visiting Our Website !!",
-    }
-
-    const info = await transporter.sendMail(content);
-    console.log("Succesfully Sent")
-}
+app.use('/.netlify/functions/server', router);
+module.exports = app;
 module.exports.handler = serverless(app);
 
 
